7b2d327855961bd8e213614c594f0a757d08c108
dmswl0311 <dmswl_0311@naver.com>
2021-01-16 17:57:08 +0900

    계수 정렬

---
+++ sorting/counting.py
@@ -0,0 +1,11 @@
+import sys
+
+array = list(map(int, sys.stdin.readline().split()))
+count = [0]*(max(array)+1)
+
+for i in range(len(array)):
+    count[array[i]] += 1
+
+for i in range(len(count)):
+    for j in range(count[i]):
+        print(i, end=' ')
