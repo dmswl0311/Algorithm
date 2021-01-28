78a29f97d6b1f70b73ec4c240a6e645cc1ad2553
dmswl0311 <dmswl_0311@naver.com>
2021-01-28 19:28:27 +0900

    실전문제_모험가 길드

---
+++ greedy/모험가 길드.py
@@ -0,0 +1,12 @@
+n = int(input())
+num = sorted(list(map(int, input().split())))
+cnt = 0
+result = 0
+
+for i in num:
+    cnt += 1
+    if cnt >= i:
+        result += 1
+        cnt = 0
+
+print(result)
