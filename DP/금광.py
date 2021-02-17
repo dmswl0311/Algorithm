a2f3372e1ae30e8408fbf6920e248f473afab46c
dmswl0311 <dmswl_0311@naver.com>
2021-02-17 23:40:28 +0900

    [DP] 금광

--- DP/금광.py
+++ DP/금광.py
@@ -1,49 +1,30 @@
 # DP
 
-t = int(input())
-result = []
-
-for _ in range(t):
+for tc in range(int(input())):
     n, m = map(int, input().split())
-    num_list = list(map(int, input().split()))
-    array = [[0]*m for _ in range(n)]
-    cnt = 0
+    array = list(map(int, input().split()))
 
+    dp = []
+    index = 0
     for i in range(n):
-        for j in range(m):
-            array[i][j] = num_list[cnt]
-            cnt += 1
-    print(array)
-    dx = [-1, 0, 1]
-    dy = [1, 1, 1]
+        dp.append(array[index:index + m])
+        index += m
 
-    next = 0
-    x = 0
-    y = 0
-    sum = 0
+    for j in range(1, m):
+        for i in range(n):
+            if i == 0:
+                left_up = 0
+            else:
+                left_up = dp[i-1][j-1]
+            if i == n-1:
+                left_down = 0
+            else:
+                left_down = dp[i+1][j-1]
+            left = dp[i][j-1]
+            dp[i][j] = dp[i][j]+max(left_up, left_down, left)
 
-    for i in range(n-1):
-        if array[i][0] < array[i+1][0]:
-            x = i+1
-        else:
-            continue
+    result = 0
+    for i in range(n):
+        result = max(result, dp[i][m-1])
 
-    sum += array[x][y]
-    print(sum)
-
-    for _ in range(m-1):
-        for i in range(3):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            if nx < n and ny < m:
-                if next < array[nx][ny]:
-                    next = array[nx][ny]
-        x = nx
-        y = ny
-        sum += next
-        print(sum)
-        next = 0
-    result.append(sum)
-
-for i in result:
-    print(i)
+    print(result)
